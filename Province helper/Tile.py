a91d7c05145ba82764ffde965bd28905c9c6b45b
khodi7 <42445258+khodi7@users.noreply.github.com>
2020-01-30 18:31:53 +0100

    Update Tile.py
    Removed pops from the file (they will now be handled by Pop.py)

--- Province helper/Tile.py
+++ Province helper/Tile.py
@@ -1,17 +1,16 @@
+import Pop
 class Tile:
     """The Tile represents a settlement or a city in Imperator: Rome (I:R).
 
     Attributes :
         buildings (list of Buildings) : buildings in the tile.
         name (str) : is the name of the tile.
-        pop (list) : is a list of ints containing the amount of each kind of pop in a tile.
-        [citizen amount, freeman amount, tribesman amount, slave amount]
-        province (Province) : is the Province in wich Tile is located
+        pop (list of Pops)
         terrain (int) : is the index of the terrain type the tile is built on in the following list :
         ["Desert", "Farmland", "Forest", "Hills", "Jungle", "Marsh", "Mountains", "Plains"]
         trade_good (TradeGood) : tile's trade good.
         
-        other_modifiers (list) : list of Modifier's.
+        other_modifiers (list of Modifiers)
     """
     
     def __init__(self, nam, prov, ter, trade_g, other = [], builds = [], pop = []):
@@ -27,9 +26,22 @@ class Tile:
         self._buildings = builds
         self._name = nam
         self._pop = pop
-        self._province = prov
         self._terrain = ter
         self._trade_good = trade_g
         self._other_modifiers = other
     
-    """
+    @property
+    def buildings(self):
+        """
+        The amount of building a tile has cannot exceed its building capacity.
+        """
+        return self._buildings
+    
+class Settlement(Tile):
+    pass
+
+class City(Tile):
+    pass
+
+class Metropolis(City):
+    pass
