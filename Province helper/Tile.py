7ea4092301fc37283fb5f75a1d50b03f9db62ddc
khodi7 <42445258+khodi7@users.noreply.github.com>
2020-01-30 17:47:20 +0100

    Updated class description

--- Province helper/Tile.py
+++ Province helper/Tile.py
@@ -2,15 +2,17 @@ class Tile:
     """The Tile represents a settlement or a city in Imperator: Rome (I:R).
 
     Attributes :
-        name (str) : is the name of the tile
-        terrain (int) : is the index of the terrain type the tile is built on in the following list :
-        ["Desert", "Farmland", "Forest", "Hills", "Jungle", "Marsh", "Mountains", "Plains"]
-        civilization (float)
-        unrest (float)
-        migration (float)
+        buildings (list of Buildings) : buildings in the tile.
+        name (str) : is the name of the tile.
         pop (list) : is a list of ints containing the amount of each kind of pop in a tile.
         [citizen amount, freeman amount, tribesman amount, slave amount]
-        buildings (list) : list of building objects.
+        province (Province) : is the Province in wich Tile is located
+        terrain (int) : is the index of the terrain type the tile is built on in the following list :
+        ["Desert", "Farmland", "Forest", "Hills", "Jungle", "Marsh", "Mountains", "Plains"]
+        trade_good (TradeGood) : tile's trade good.
+        
+        other_modifiers (list) : list of Modifier's.
+    """
     
     
     """
