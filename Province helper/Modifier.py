d6786e56602f933d0579ae917f9d30395be1cdca
khodi7 <42445258+khodi7@users.noreply.github.com>
2020-02-05 18:48:02 +0100

    Finished making accessors

--- Province helper/Modifier.py
+++ Province helper/Modifier.py
@@ -15,16 +15,27 @@ class Modifier:
         I.e. : (commerce_output1 + commerce_output2) * (commerce_modifier1 + commerce_modifier2) = total_commerce
     """
     
-    def __init__(self):
+    def __init__(self, co = 0.0, cm = 0.0, mo = 0.0, mm = 0.0, ro = 0.0, rm = 0.0, to = 0.0, tm = 0.0):
+        """
+        Args :
+            co(float) : commerce output
+            cm(float) : commerce modifier
+            mo(float) : manpower output
+            mm(float) : manpower modifier
+            ro(float) : research output
+            rm(float) : research modifier
+            to(float) : tax output
+            tm(float) : tax modifier
+        """
         
-        self._commerce_output = 0
-        self._commerce_modifier = 0
-        self._manpower_output = 0
-        self._manpower_modifier = 0
-        self._research_output = 0
-        self._research_modifier = 0
-        self._tax_output = 0
-        self._tax_modifier = 0
+        self._commerce_output = co
+        self._commerce_modifier = cm
+        self._manpower_output = mo
+        self._manpower_modifier = mm
+        self._research_output = ro
+        self._research_modifier = rm
+        self._tax_output = to
+        self._tax_modifier = tm
     
     @property
     def commerce_output(self):
@@ -65,3 +76,27 @@ class Modifier:
     @research_output.setter
     def research_output(self, n):
         self._research_output = n
+        
+    @property
+    def research_modifier(self):
+        return self._research_modifier
+    
+    @research_modifier.setter
+    def research_modifier(self, n):
+        self._research_modifier = n
+        
+    @property
+    def tax_output(self):
+        return self._tax_output
+    
+    @tax_output.setter
+    def tax_output(self, n):
+        self._tax_output = n
+        
+    @property
+    def tax_modifier(self):
+        return self._tax_modifier
+    
+    @tax_modifier.setter
+    def tax_modifier(self, n):
+        self._tax_modifier = n
