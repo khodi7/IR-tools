baa15ea55e2459ec269c6c2737dcc45045f648b3
khodi7 <42445258+khodi7@users.noreply.github.com>
2020-02-15 11:18:45 +0100

    Modifier updated
    Moved the building class and its children in the modifier file.
    Added trade goods too.
    Replaced output modifiers by pop output modifiers.

--- Province helper/Modifier.py
+++ Province helper/Modifier.py
@@ -2,101 +2,370 @@ class Modifier:
     """Regroup the modifiers affecting ressource production.
     
     Attributes:
-        commerce_output(float)
+        citizen_output(float)
         commerce_modifier(float)
-        manpower_output(float)
+        freemen_output(float)
         manpower_modifier(float)
-        research_output(float)
+        slaves_output(float)
         research_modifier(float)
-        tax_output(float)
         tax_modifier(float)
-        The sum of the pops base output of each type (commerce, manpower,...) shall be multiplied by
-        the product of sum of the _modifier by the sum of the _output.
-        I.e. : (commerce_output1 + commerce_output2) * (commerce_modifier1 + commerce_modifier2) = total_commerce
+        tribesmen_output(float)
     """
     
-    def __init__(self, co = 0.0, cm = 0.0, mo = 0.0, mm = 0.0, ro = 0.0, rm = 0.0, to = 0.0, tm = 0.0):
+    def __init__(self, co = 0.0, cm = 0.0, fo = 0.0, mm = 0.0, so = 0.0, rm = 0.0, to = 0.0, tm = 0.0):
         """
         Args :
-            co(float) : commerce output
+            co(float) : citizen output
             cm(float) : commerce modifier
-            mo(float) : manpower output
+            fo(float) : freemen output
             mm(float) : manpower modifier
-            ro(float) : research output
+            so(float) : slaves output
             rm(float) : research modifier
-            to(float) : tax output
+            to(float) : tribesmen output
             tm(float) : tax modifier
         """
         
-        self._commerce_output = co
+        self._citizen_output = co
         self._commerce_modifier = cm
-        self._manpower_output = mo
+        self._freemen_output = fo
         self._manpower_modifier = mm
-        self._research_output = ro
+        self._slaves_output = so
         self._research_modifier = rm
-        self._tax_output = to
+        self._tribesmen_output = to
         self._tax_modifier = tm
     
-    @property
-    def commerce_output(self):
-        return self._commerce_output
+#     @property
+#     def citizen_output(self):
+#         return self._citizen_output
+#     
+#     @citizen_output.setter
+#     def citizen_output(self, n):
+#         self._citizen_output = n
+#         
+#     @property
+#     def commerce_modifier(self):
+#         return self._commerce_modifier
+#     
+#     @commerce_modifier.setter
+#     def commerce_modifier(self, n):
+#         self._commerce_modifier = n
+#         
+#     @property
+#     def freemen_output(self):
+#         return self._manpower_output
+#     
+#     @manpower_output.setter
+#     def manpower_output(self, n):
+#         self._manpower_output = n
+#         
+#     @property
+#     def manpower_modifier(self):
+#         return self._manpower_modifier
+#     
+#     @manpower_modifier.setter
+#     def manpower_modifier(self, n):
+#         self._manpower_modifier = n
+#         
+#     @property
+#     def research_output(self):
+#         return self._research_output
+#     
+#     @research_output.setter
+#     def research_output(self, n):
+#         self._research_output = n
+#         
+#     @property
+#     def research_modifier(self):
+#         return self._research_modifier
+#     
+#     @research_modifier.setter
+#     def research_modifier(self, n):
+#         self._research_modifier = n
+#         
+#     @property
+#     def tribesmen_output(self):
+#         return self._tribesmen_output
+#     
+#     @tribesmen_output.setter
+#     def tribesmen_output(self, n):
+#         self._tribesmen_output = n
+#         
+#     @property
+#     def tax_modifier(self):
+#         return self._tax_modifier
+#     
+#     @tax_modifier.setter
+#     def tax_modifier(self, n):
+#         self._tax_modifier = n
+        
+        
+class ModifyingAgent():
+    """Object containing a modifier
     
-    @commerce_output.setter
-    def commerce_output(self, n):
-        self._commerce_output = n
+    Attributes:
+        amount(int): the amount of modifying agents.
+        modifier(Modifier): the modifier linked to it.
+        name(string): its name.
+    """
+    
+    def __init__(self, n):
+        self.amount = n
+        self._modifier = None
+        self._name = None
         
     @property
-    def commerce_modifier(self):
-        return self._commerce_modifier
+    def modifier(self):
+        return self._modifier
     
-    @commerce_modifier.setter
-    def commerce_modifier(self, n):
-        self._commerce_modifier = n
+    @property
+    def name(self):
+        return self._name
+    
+    
+#--------Buildings--------#
+
+
+class BuildingGroup(ModifyingAgent):
+    """BuildingGroup is used to represent a group of buildings from I:R
+
+    Attributes:
+        type(int): indicates if the building can be built in settlements
+        and/or cities. 0 for settlement, 1 for city and 2 for both.
+    """
+    
+    def __init__(self, n):
+        super.__init__(n)
+        self._name = "Template building"
+        self._type = 2
+    
+    @property
+    def type(self):
+        return self.type
+
+
+class TrainingCampGroup(BuildingGroup):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier.Modifier(mm = 0.1)
+        self._name = "Training camp"
+        self._type = 1
+
+
+class BarracksGroup(BuildingGroup):
+    # Unimplemented happiness and pop capacity
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier.Modifier(mm = 0.2)
+        self._name = "Barrack"
+        self._type = 0
+
+
+# Check how the building works ingame
+# class FoundryGroup(BuildingGroup):
+#     
+#     def __init__(self, n):
+#         super().__init__(n)
+#         self._modifier = Modifier.Modifier(co = 0.01, mo = 0.01, ro = 0.01, to = 0.01)
+#         self._name = "Foundry"
+#         self._type = 1
+
+
+class MarketGroup(BuildingGroup):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier.Modifier(cm = 0.25)  # Not sure. Check in case of bug
+        self._name = "Market"
+        self._type = 1
+
+
+class TaxOfficeGroup()
+    def __init__(self, n)
+        super().__init__(n)
+        self._modifier = Modifier.Modifier(tm = 0.1)
+        self._name = "Tax office"
+        self._type = 1
+        
+
+class AcademyGroup()
+    def __init__(self, n)
+        super().__init__(n)
+        self._modifier = Modifier.Modifier(rm = 0.075)
+        self._name = "Academy"
+        self._type = 1
+        
+
+#--------Trade goods--------#
+
+
+class TradeGood(ModifyingAgent):
+    """Trade good from I:R
+    
+    Attributes:
+        amount(int)
+        modifier(Modifier): Represents the modifier the first unit of the trade good
+        gives.
+        modifier2(Modifier): Represents the modifier one more unit of the trade good
+        gives in the province capital.
+        name(string)
+    """
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier2 = None
+        self._name = "Template trade good"
         
     @property
-    def manpower_output(self):
-        return self._manpower_output
+    def modifier2(self):
+        return self._modifier2
+
+
+class Amber(TradeGood):
     
-    @manpower_output.setter
-    def manpower_output(self, n):
-        self._manpower_output = n
-        
-    @property
-    def manpower_modifier(self):
-        return self._manpower_modifier
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(tm = 0.1)
+        self._modifier2 = Modifier(tm = 0.05)
+        self._name = "Amber"
+
+
+class BaseMetals(TradeGood):
     
-    @manpower_modifier.setter
-    def manpower_modifier(self, n):
-        self._manpower_modifier = n
-        
-    @property
-    def research_output(self):
-        return self._research_output
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(fo = 0.05)
+        self._modifier2 = Modifier(fo = 0.025)
+        self._name = "Base metals"
+
+
+class Cloth(TradeGood):
     
-    @research_output.setter
-    def research_output(self, n):
-        self._research_output = n
-        
-    @property
-    def research_modifier(self):
-        return self._research_modifier
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(tm = 0.1)
+        self._modifier2 = Modifier(tm = 0.05)
+        self._name = "Cloth"
+
+
+class Earthware(TradeGood):
     
-    @research_modifier.setter
-    def research_modifier(self, n):
-        self._research_modifier = n
-        
-    @property
-    def tax_output(self):
-        return self._tax_output
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(rm = 0.05)
+        self._modifier2 = Modifier(rm = 0.01)
+        self._name = "Earthware"
+
+
+class Elephants(TradeGood):
     
-    @tax_output.setter
-    def tax_output(self, n):
-        self._tax_output = n
-        
-    @property
-    def tax_modifier(self):
-        return self._tax_modifier
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier2 = Modifier(mm = 0.05)
+        self._name = "Elephants"
+
+
+class Gems(TradeGood):
     
-    @tax_modifier.setter
-    def tax_modifier(self, n):
-        self._tax_modifier = n
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(tm = 0.2)
+        self._modifier2 = Modifier(tm = 0.1)
+        self._name = "Gems"
+
+
+class Hemp(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(so = 0.1)
+        self._modifier2 = Modifier(so = 0.02)
+        self._name = "Hemp"
+
+
+class Honey(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(fo = 0.05)
+        self._modifier2 = Modifier(fo = 0.025)
+        self._name = "Honey"
+
+
+class Incense(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(cm = 0.1)
+        self._modifier2 = Modifier(cm = 0.02)
+        self._name = "Incense"
+
+
+class Iron(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier2 = Modifier(mm = 0.05)
+        self._name = "Iron"
+
+
+class Leather(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(so = 0.1)
+        self._modifier2 = Modifier(so = 0.02)
+        self._name = "Leather"
+
+
+class Papyrus(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(rm = 0.2)
+        self._modifier2 = Modifier(co = 0.05)
+        self._name = "Papyrus"
+
+
+class Silk(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(cm = 0.15)
+        self._modifier2 = Modifier(cm = 0.05)
+        self._name = "Silk"
+
+
+class Spices(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(co = 0.05)
+        self._modifier2 = Modifier(co = 0.025)
+        self._name = "Spices"
+
+
+class SteppeHorses(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier2 = Modifier(mm = 0.05)
+        self._name = "Steppe horses"
+
+
+class WildGames(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier = Modifier(to = 0.1)
+        self._modifier2 = Modifier(to = 0.05)
+        self._name = "Spices"
+
+
+class Wood(TradeGood):
+    
+    def __init__(self, n):
+        super().__init__(n)
+        self._modifier2 = Modifier(tm = 0.03)
+        self._name = "Wood"
